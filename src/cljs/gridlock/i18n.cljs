0bf46887a79f27f2b04f74f38303514365c932a2
Daniel Janus <dj@danieljanus.pl>
2019-12-30 17:31:10 +0100

    Add i18n namespace.

---
+++ src/cljs/gridlock/i18n.cljs
@@ -0,0 +1,33 @@
+(ns gridlock.i18n)
+
+(def i18n
+  {:pl
+   {:hint "Podpowiedź"
+    :reset "Resetuj"
+    :new-game "Nowa gra"
+    :hello "Witaj!"
+    :start-game "Zacznij grę"
+    :how-to-play "Jak grać?"
+    :hooray "Brawo!"
+    :play-again "Jeszcze raz"
+    :select-difficulty "Wybierz poziom trudności:"
+    :easy "Łatwy"
+    :normal "Średni"
+    :hard "Trudny"
+    :loading-dictionaries "Ładuję słowniki"
+    :written-in-cljs "Napisane w języku ClojureScript"}
+   :en
+   {:hint "Hint"
+    :reset "Reset"
+    :new-game "New game"
+    :hello "Hello!"
+    :start-game "Start game"
+    :how-to-play "How to play"
+    :hooray "Hooray!"
+    :play-again "Play again"
+    :select-difficulty "Select difficulty:"
+    :easy "Easy"
+    :normal "Normal"
+    :hard "Hard"
+    :loading-dictionaries "Loading dictionaries"
+    :written-in-cljs "Written in ClojureScript"}})
